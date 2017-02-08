5d48d66745671f272f6d12901f296c1a058b8588
Perry Sidler <sidle104@mail.chapman.edu>
2017-02-08 15:39:50 -0800

    add rao_converter.py, converts strings to raolang strings

---
+++ rao_converter.py
@@ -0,0 +1,38 @@
+# Converts strings to raolang strings
+def convert(string):
+    converted = ''
+
+    for char in string:
+        converted += 'rao@'
+        val = ord(char)
+        valstr = str(val)
+        place = 100 if len(valstr) == 3 else 10 if len(valstr) == 2 else 1
+
+        # each iteration is 1 place in the ascii value
+        for i in range(len(valstr)):
+            num = int(valstr[i])
+            for n in range(num):
+                if place == 100:
+                    converted += '!'
+                elif place == 10:
+                    converted += ','
+                else:
+                    converted += '.'
+            # advance to the next place
+            place = place / 10
+            
+    return converted
+
+f = open('rao_converted.txt', 'a')
+running = True
+
+while running:
+    user_input = input('Convert: ')
+
+    if user_input == 'rao:)':
+        running = False
+        f.close()
+        break
+    
+    print('Success!')
+    f.write('{0} => {1}\n'.format(user_input, convert(user_input)))
